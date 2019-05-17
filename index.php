74de602f802f0c9ba14c8429434bc11b604d4b8f
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-05-17 16:29:37 +0200

    Added index and css files

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" type="text/css" href="main.css">
+    <title>The Book Club</title>
+</head>
+<body>
+    <div id="main_div">
+        <h1>The Book Club</h1>
+    </div>
+</body>
+</html>
