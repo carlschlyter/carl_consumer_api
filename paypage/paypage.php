dae9b2e51782461a4221084f3a45d3155b29c1ac
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2019-05-28 15:07:50 +0200

    Working with stripe payment part

--- paypage/paypage.php
+++ paypage/paypage.php
@@ -28,6 +28,7 @@
       <button>Submit Payment</button>
     </form>
 </div>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
     <script src="https://js.stripe.com/v3/"></script>
     <script src="./js/charge.js"></script>
 </body>
