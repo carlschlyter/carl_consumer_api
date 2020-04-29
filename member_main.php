2c1faf290930b4e450e9716a5ea997fac785675c
Carl Schlyter <carl.olof.schlyter@hotmail.com>
2020-04-29 14:16:47 +0200

    Refactored the code a bit to remove small errors

--- member_main.php
+++ member_main.php
@@ -29,7 +29,7 @@
 
     //Check if there is an uploaded file of the csv type and in that case move it from the temp folder to the uploads folder within this project. 
 
-    if (isset($_FILES)) {
+    if (isset($_FILES['fileToUpload'])) {
         $check = true;
 
         if ($_FILES['fileToUpload']['type'] !== 'application/vnd.ms-excel'){
@@ -43,50 +43,47 @@
             // echo ($path . '/uploads/');
             // print_r($_FILES['fileToUpload']['tmp_name']);
             move_uploaded_file($_FILES['fileToUpload']['tmp_name'], "$path");
+
+            //Create the empty array, "$books", fill it first with the captions of each column in the new file to be. Then open the uploaded file for reading and if there is csv data in it, put it in the $data variable and use the complete_book function to put the csv data in the $books array on the index 0 position, being the isbn column. 
+
+            $filename = $path;
+            $books = [];
+            // print_r($books);
+            $books[] = ['Isbn', 'Title', 'Author', 'Publisher'];
+            // print_r($books)
+
+            if ($file_handle = fopen($filename, 'r')) {
+                while ($data = fgetcsv($file_handle)){
+                    $books[] = complete_book($data[0]);   
+                    // print_r($data);         
+                }
+                fclose($file_handle);
+            }
+
+                //If there now is a $books array at all, create a new file in the uploads folder, "new_books.csv", and open it for writing. Loop through each book in the $books array and write the data to new new file.   
+
+            if($books){
+                $file_to_write = fopen('uploads/new_books.csv', 'w');
+
+                $all_is_good = true;
+
+                foreach($books as $book){
+
+                        $all_is_good = $all_is_good && fputcsv($file_to_write, $book, ';');
+                
+                }
+
+                fclose($file_to_write);
+
+                    // print_r($book);
+
+                if ($all_is_good){
+                    ?><p class="center"><?php echo '<a href="uploads/new_books.csv">Your completed book file!</a>';?></p><?php
+                } else {
+                    echo 'Something happened';
+                }
+            }
         }
-    }
-
-    //Create the empty array, "$books", fill it first with the captions of each column in the new file to be. Then open the uploaded file for reading and if there is csv data in it, put it in the $data variable and use the complete_book function to put the csv data in the $books array on the index 0 position, being the isbn column. 
-
-    $filename = $path;
-    $books = [];
-    // print_r($books);
-    $books[] = ['Isbn', 'Title', 'Author', 'Publisher'];
-    // print_r($books);
-
-    if ($file_handle = fopen($filename, 'r')) {
-        while ($data = fgetcsv($file_handle)){
-            $books[] = complete_book($data[0]);   
-            // print_r($data);         
-        }
-        fclose($file_handle);
-    }
-    // print_r($data);          
-    // print_r($books);
-
-    //If there now is a $books array at all, create a new file in the uploads folder, "new_books.csv", and open it for writing. Loop through each book in the $books array and write the data to new new file.   
-
-    if($books){
-        $file_to_write = fopen('uploads/new_books.csv', 'w');
-
-        $all_is_good = true;
-
-        foreach($books as $book){
-
-                $all_is_good = $all_is_good && fputcsv($file_to_write, $book, ';');
-         
-        }
-
-        fclose($file_to_write);
-
-            // print_r($book);
-
-        if ($all_is_good){
-            ?><p class="center"><?php echo '<a href="uploads/new_books.csv">Your completed book file!</a>';?></p><?php
-        } else {
-            echo 'Something happened';
-        }
-
     }
 
     
